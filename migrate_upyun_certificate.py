a4f61d65db5b8153db5b8f432a4a2f095c01ff8c
wenjun <hi@awen.me>
2021-07-16 16:16:20 +0800

    Update migrate_upyun_certificate.py

--- migrate_upyun_certificate.py
+++ migrate_upyun_certificate.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
+import logging
+import sys
+
 from requests import Session
 from time import time, sleep
 
@@ -16,6 +19,19 @@ class MigrateUpyunCertificate:
 
     def __init__(self):
         self.session = Session()
+        self.logger = logging.getLogger("upyun")
+        formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+        # 文件日志
+        file_handler = logging.FileHandler("upyun.log")
+        file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
+        # 控制台日志
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.formatter = formatter  # 也可以直接给formatter赋值
+        # 为logger添加的日志处理器
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
+        # 指定日志的最低输出级别，默认为WARN级别
+        self.logger.setLevel(logging.INFO)
 
     def login(self, username, password):
         """
@@ -189,7 +205,8 @@ class MigrateUpyunCertificate:
             return False
         resp_json = response.json()
         if resp_json["data"]["result"]:
-            print("证书迁移结果 {}".format(resp_json["data"]["result"]))
+            result_data = resp_json["data"]["result"]
+            self.logger.info(f"证书迁移结果 {result_data}")
             return True
 
     def delete_certificate(self, certificate_id, by_time):
@@ -200,8 +217,10 @@ class MigrateUpyunCertificate:
         :param certificate_id:
         :return:
         """
-        if by_time < 0:
+        if by_time >= 1:
+            self.logger.info("当前证书大于 2天，无需删除")
             return
+        self.logger.info("小于 1 天，开始删除旧证书")
         url = "https://console.upyun.com/api/https/certificate/?certificate_id={}".format(certificate_id)
         headers = {
             'Accept': 'application/json, text/plain, */*',
@@ -223,59 +242,61 @@ class MigrateUpyunCertificate:
 
         response = self.session.delete(url, headers=headers)
         if response.status_code != 200:
-            print("证书删除失败，连接异常")
+            self.logger.info("证书删除失败，连接异常")
         resp_json = response.json()
         try:
             if resp_json["data"]["status"]:
-                print("证书删除成功")
+                self.logger.info("证书删除成功")
         except KeyError:
-            print("证书删除失败")
+            self.logger.info("证书删除失败")
 
     @staticmethod
     def read_acme_conf(conf_path, unix_time):
         with open(conf_path) as f:
             for i in (f.readlines()):
                 key = i.strip().split("=")
-                if key[0] == "Le_CertCreateTime":
-                    le_cert_create_time = str(key[1]).replace("'", "")
-                    return int(le_cert_create_time) - unix_time
+                if key[0] == "Le_NextRenewTime":
+                    le_next_renew_time = str(key[1]).replace("'", "")
+                    return int(int(le_next_renew_time) - unix_time) / 60 / 60 / 24
 
     def main(self):
 
         """############# 配置信息段 #############"""
-        username = "upyun_username"  # 又拍云用户名，必填
-        password = "upyun_password"  # 又拍云密码，必填
+        username = "fangwenjun"  # 又拍云用户名，必填
+        password = "XXXX"  # 又拍云密码，必填
         check_domain = "awen.me"  # 要配置的证书，避免误删其他过期证书，必填
-        certificate_path = "/usr/local/nginx/conf/ssl/awen.me/fullchain.cer"  # 证书公钥，建议配置待根证书的证书内容，否则在部分浏览器可能会出现问题。必填
-        private_key_path = "/usr/local/nginx/conf/ssl/awen.me/awen.me.key"  # 证书私钥，必填
-        domain_conf_path = "/usr/local/nginx/conf/ssl/awen.me/awen.me.conf"  # 证书更新配置文件，用户读取下一次更新时间
+        certificate_path = "/Users/wenjun/.acme.sh/awen.me/fullchain.cer"  # 证书公钥，建议配置待根证书的证书内容，否则在部分浏览器可能会出现问题。必填
+        private_key_path = "/Users/wenjun/.acme.sh/awen.me/awen.me.key"  # 证书私钥，必填
+        domain_conf_path = "/Users/wenjun/.acme.sh/awen.me/awen.me.conf"  # 证书更新配置文件，用户读取下一次更新时间
         """############# 配置信息段 #############"""
         unix_time = int(time())
         update_time = int(self.read_acme_conf(domain_conf_path, unix_time))
-        print(update_time)
-        print("距离下一次更新还有 {} 天".format(update_time))
-        if 0 < update_time < -86400:
-            print("还早，不要这么急嘛！")
+        self.logger.info("距离下一次更新还有 {} 天".format(update_time))
+        if update_time >= 1:
+            self.logger.info("还早，不要这么急嘛！")
             return
-        print("开始登录又拍云")
+        self.logger.info("开始登录又拍云")
         self.login(username=username, password=password)
-        print("开始获取证书列表")
+        self.logger.info("开始获取证书列表")
         result_json = self.get_ssl_list()
         cer_list = self.format_result_info(result_json, check_domain)
         old_cer_id = cer_list[-1]["cer_id"]
         validity_end = cer_list[-1]["validity_end"]
-        unix_time = unix_time * 1000
-        by_time = unix_time - int(validity_end)
-        print("开始读取本地证书公钥和私钥信息")
-        print("开始上传证书")
+        now_unix = unix_time * 1000
+        by_time = int((int(validity_end) - now_unix) / 1000 / 60 / 60 / 24)
+        self.logger.info(f"当前时间 {now_unix}, 证书截止时间 {validity_end} 剩余时间 {by_time} 天")
+        if by_time >= 1:
+            self.logger.info("当前证书大于 2天，无需操作")
+            return
+        self.logger.info("开始读取本地证书公钥和私钥信息")
+        self.logger.info("开始上传证书")
         upload_cerfile_info = self.upload_cerfile(certificate_path=certificate_path,
                                                   private_key_path=private_key_path)
         new_cer_id = upload_cerfile_info["certificate_id"]
-        print("开始迁移证书")
+        self.logger.info("开始迁移证书")
         self.migrate_certificate(new_cer_id=new_cer_id, old_cer_id=old_cer_id)
-        sleep(20)
-        print(by_time)
-        print("开始删除已过期的 {} 证书".format(check_domain))
+        sleep(5)
+        self.logger.info(f"开始删除已过期的 {check_domain} 证书,sold_cer_id {old_cer_id},by_time {by_time}")
         self.delete_certificate(old_cer_id, by_time)
 
 
